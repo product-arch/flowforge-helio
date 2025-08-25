9dbe5a803c973ed7686a525d4cd42191f552a6ee
product-arch <product@helo.ai>
2025-08-25 13:24:58 +0530

    Convert swagger server to ES modules

--- src/server/swagger-server.js
+++ src/server/swagger-server.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import yaml from 'js-yaml';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 8080;
